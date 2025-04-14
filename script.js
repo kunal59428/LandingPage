224f60e32f19214ed0099b1692cae5473bf18b55
Ashutosh741 <kumarsharmakunal@gmail.com>
2025-04-14 14:48:46 +0530

    work details updated

--- script.js
+++ script.js
@@ -68,34 +68,34 @@ var swiper = new Swiper(".slide-content", {
 
 // Testimonial
 
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    items: 3,
-    autoplay: true,
-    margin: 30,
-    loop: true,
-    dots: true,
-    // nav: true,
-    // navText: ["<i class='fas fa-long-arrow-alt-left'></i>", "<i class='fas fa-long-arrow-alt-right'></i>"],
-    responsive: {
-      0: {
-        items: 1
-      },
-      576: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      992: {
-        items: 3
-      },
-      1200: {
-        items: 3
-      }
-    }
-  });
-});
+// $(document).ready(function(){
+//   $(".owl-carousel").owlCarousel({
+//     items: 3,
+//     autoplay: true,
+//     margin: 30,
+//     loop: true,
+//     dots: true,
+//     // nav: true,
+//     // navText: ["<i class='fas fa-long-arrow-alt-left'></i>", "<i class='fas fa-long-arrow-alt-right'></i>"],
+//     responsive: {
+//       0: {
+//         items: 1
+//       },
+//       576: {
+//         items: 1
+//       },
+//       768: {
+//         items: 2
+//       },
+//       992: {
+//         items: 3
+//       },
+//       1200: {
+//         items: 3
+//       }
+//     }
+//   });
+// });
 
 
 // PopUp Form
